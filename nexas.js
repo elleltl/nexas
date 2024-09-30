512145118b3e89ff829329d7833f9d680980e224
elleltl <1811seraph@gmail.com>
2024-09-30 21:37:58 +1000

    Add files via upload

---
+++ nexas.js
@@ -0,0 +1,154 @@
+var metadata = {
+  "Alvyn Asher": {
+    image: "img/alvyn.png",
+  },
+  "Aurix ‘Rixxy’ Brightful": {
+    image: "img/rixxy.png",
+  },
+  "Bakurah Noir": {
+    image: "img/baku.png",
+  },
+  "Charlie Morgan": {
+    image: "img/charlie.png",
+  },
+  "Cheryl Alcmene": {
+    image: "img/cheryl.png",
+  },
+  "Clotho v Melodiam": {
+    image: "img/clotho.png",
+  },
+  "Cora Light": {
+    image: "img/cora.png",
+  },
+  "Enaria Liliales": {
+    image: "img/enaria.png",
+  },
+  "Endra Jiéshù": {
+    image: "img/endra.png",
+  },
+  “Esila Shephield”: {
+    image: "img/esila.png",
+  },
+  "Faelie Fallwood": {
+    image: "img/faelie.png",
+  },
+  "Hikari Kogane": {
+    image: "img/hikari.png",
+  },
+  "Izuya Kagamiya": {
+    image: "img/izuya.png",
+  },
+  "Kanami Kaito": {
+    image: "img/kaito.png",
+  },
+  “Kenzo Kozima”: {
+    image: "img/kenzo.png",
+  },
+  “Kiko Hayashi”: {
+    image: "img/kiko.png",
+  },
+  "Kougami Harue": {
+    image: "img/harue.png",
+  },
+  "Kyohei Tekma": {
+    image: "img/kyohei.png",
+  },
+  “Leobel Purson”: {
+    image: "img/leobel.png",
+  },
+  “Lulu Lasso”: {
+    image: "img/lulu.png",
+  },
+  “Lumiora Lumen”: {
+    image: "img/lumi.png",
+  },
+  "Manon Merope": {
+    image: "img/manon.png",
+  },
+  “Melody Cordelia”: {
+    image: "img/melody.png",
+  },
+  "Mia Fenimore": {
+    image: "img/mia.png",
+  },
+  "Mizuki Akemi": {
+    image: "img/mizuki.png",
+  },
+  “Moka Roki”: {
+    image: "img/moka.png",
+  },
+  "Momoko Nuihara": {
+    image: "img/momo.png",
+  },
+  "Nano Gurē": {
+    image: "img/nano.png",
+  },
+  Nell: {
+    image: "img/nell.png",
+  },
+  “Noah Marcellios”: {
+    image: "img/noah.png",
+  },
+  "Nowi Inari": {
+    image: "img/nowi.png",
+  },
+  "Nox Orphee": {
+    image: "img/nox.png",
+  },
+  "Nyantsume": {
+    image: "img/nyantsume.png",
+  },
+  “Odette Mori”: {
+    image: "img/odette.png",
+  },
+  “Osian W.D. Whitlock”: {
+    image: "img/osian.png",
+  },
+  “Perseus Mavros”: {
+    image: "img/percy.png",
+  },
+  "Piko Halo": {
+    image: "img/piko.png",
+  },
+  “Poette Evergreen”: {
+    image: "img/poette.png",
+  },
+  "Pyon Phelix": {
+    image: "img/phelix.png",
+  },
+  Reverie: {
+    image: "img/reverie.png",
+  },
+  "Roseli Petale": {
+    image: "img/roseli.png",
+  },
+  "Rov Alcou": {
+    image: "img/rov.png",
+  },
+  “Rune Atelier”: {
+    image: "img/rune.png",
+  },
+  “Ruri Tinytale”: {
+    image: "img/ruri.png",
+  },
+  "Silas Ain": {
+    image: "img/silas.png",
+  },
+  "Soma Fujimori": {
+    image: "img/soma.png",
+  },
+  “Sweet E. Hiroshi”: {
+    image: "img/sweet.png",
+  },
+  "Terryn Shieldsong": {
+    image: "img/terryn.png",
+  },
+  “Vera Avantgarde”: {
+    image: "img/vera.png",
+  },
+  “Wendy Wanderful”: {
+    image: "img/wendy.png",
+  },
+  “Yukiko Yasashi”: {
+    image: "img/yuki.png",
+};
